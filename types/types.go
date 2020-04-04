e6779735e45e457a5f59cf743ca241d819d434cf
Rashed Mohammed <25506588+rashedmyt@users.noreply.github.com>
2020-04-04 12:52:06 +0530

    added private and public key types

---
+++ types/types.go
@@ -0,0 +1,9 @@
+// Copyright 2020 The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+package types
+
+type PrivateKey [32]byte
+
+type PublicKey [32]byte
