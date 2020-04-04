73ff7169c52c90c97b35b3b2858e3e2bca65ff44
Rashed Mohammed <25506588+rashedmyt@users.noreply.github.com>
2020-04-04 12:55:23 +0530

    added mnemonics code

---
+++ walletbackend/mnemonics/mnemonic_test.go
@@ -0,0 +1,56 @@
+// Copyright 2020 The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+package mnemonics
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/turtlecoin/go-turtlecoin/types"
+	"github.com/turtlecoin/go-turtlecoin/utils"
+)
+
+var tests = []struct {
+	seed string
+	key  string
+}{
+	{
+		"cement wallets dwarf karate directed surfer insult logic desk spout pipeline inroads elapse mobile syringe launching pool ravine bomb eagle happens poetry toilet touchy syringe",
+		"68367745ff0af79e7b1b24b145e398c215de1c4a96f76f079fc2af22b2349b01",
+	},
+	{
+		"semifinal ailments punch federal army faked bobsled anvil incur ticket having bias bamboo bagpipe lair arena utility affair wrong total lair directed taunts terminal bagpipe",
+		"8bb480a81cdf1e37f253055cc4a02fbd90de7d624ec18119d18b349b1e1c2602",
+	},
+	{
+		"sulking testing unbending dexterity mobile neon lazy argue wobbly mumble pyramid legion eternal pool eggs woven upcoming southern jukebox hectare females emulate weekday wolf emulate",
+		"b6c9d20bef4d250786de94ea5b3f4fcb6fbb3391be6d6fe587641beb953f8604",
+	},
+}
+
+func TestMnemonicToPrivateKey(t *testing.T) {
+	for _, test := range tests {
+		key, err := MnemonicToPrivateKey(test.seed)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if hex.EncodeToString(key[:]) != test.key {
+			t.Errorf("Invalid conversion from mnemonic to private key: got %s want %s", key, test.key)
+		}
+	}
+}
+
+func TestPrivateKeyToMnemonic(t *testing.T) {
+	for _, test := range tests {
+		var testKey types.PrivateKey
+		keyBytes, _ := utils.DecodeHex(test.key)
+		copy(testKey[:], keyBytes)
+		seed := PrivateKeyToMnemonic(testKey)
+		if seed != test.seed {
+			t.Errorf("Invalid conversion from private key to mnemonic: got %s want %s", seed, test.seed)
+		}
+	}
+}
---
+++ walletbackend/mnemonics/mnemonics.go
@@ -0,0 +1,124 @@
+// Copyright 2020 The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+package mnemonics
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"strings"
+
+	"github.com/turtlecoin/go-turtlecoin/types"
+)
+
+func MnemonicToPrivateKey(seed string) (privateKey types.PrivateKey, err error) {
+	words := strings.Split(seed, " ")
+	wLen := len(words)
+
+	if wLen < 25 {
+		var wordPlural string
+
+		if wLen == 1 {
+			wordPlural = "word"
+		} else {
+			wordPlural = "words"
+		}
+
+		err = fmt.Errorf("The mnemonic seed given is of wrong length. It"+
+			" should be 25 words long, but it is %d %s long.", wLen, wordPlural)
+		return privateKey, err
+	}
+
+	wIndices, err := getWordIndices(words)
+	if err != nil {
+		return privateKey, err
+	}
+
+	if !hasValidChecksum(words) {
+		return privateKey, errors.New("Given mnemonic seed has invalid checksum")
+	}
+
+	wlLen := len(english)
+
+	for i := 0; i < (wLen-1)/3; i++ {
+		w1 := wIndices[i*3]
+		w2 := wIndices[i*3+1]
+		w3 := wIndices[i*3+2]
+
+		val := w1 + wlLen*(((wlLen-w1)+w2)%wlLen) + wlLen*wlLen*(((wlLen-w2)+w3)%wlLen)
+
+		if val%wlLen != w1 {
+			return privateKey, errors.New("Invalid mnemonic")
+		}
+
+		binary.LittleEndian.PutUint32(privateKey[i*4:], uint32(val))
+	}
+
+	return
+}
+
+func PrivateKeyToMnemonic(privateKey types.PrivateKey) string {
+	var words []string
+	wlLen := uint32(len(english))
+
+	for i := 0; i < 31; i += 4 {
+		val := binary.LittleEndian.Uint32(privateKey[i:])
+
+		w1 := val % wlLen
+		w2 := ((val / wlLen) + w1) % wlLen
+		w3 := (((val / wlLen) / wlLen) + w2) % wlLen
+
+		words = append(words, english[w1])
+		words = append(words, english[w2])
+		words = append(words, english[w3])
+	}
+
+	words = append(words, getChecksumWord(words))
+
+	return strings.Join(words, " ")
+}
+
+func hasValidChecksum(words []string) bool {
+	return words[len(words)-1] == getChecksumWord(words[:len(words)-1])
+}
+
+func getChecksumWord(words []string) string {
+	var trimmed string
+
+	for _, v := range words {
+		trimmed += v[0:3]
+	}
+
+	hash := crc32.ChecksumIEEE([]byte(trimmed))
+
+	return words[hash%uint32(len(words))]
+}
+
+func getWordIndices(words []string) ([]int, error) {
+
+	// Create a map to retrieve indices
+	indices := map[string]int{}
+
+	maxLen := len(english)
+
+	for i := 0; i < maxLen; i++ {
+		indices[english[i]] = i
+	}
+
+	res := make([]int, len(words))
+
+	for i, v := range words {
+		if index, ok := indices[v]; ok {
+			res[i] = index
+		} else {
+			err := fmt.Errorf("The mnemonic seed given has a word that is not"+
+				" present in the english word list (%s).", v)
+			return nil, err
+		}
+	}
+
+	return res, nil
+}
