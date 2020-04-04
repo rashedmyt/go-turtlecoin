bad12f8474418a022e012866c6d909c207f38c8e
Rashed Mohammed <25506588+rashedmyt@users.noreply.github.com>
2020-04-04 12:56:31 +0530

    basic walletbackend structure

---
+++ walletbackend/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2020 The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+package walletbackend
---
+++ walletbackend/walletbackend.go
@@ -0,0 +1,10 @@
+// Copyright 2020 The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+package walletbackend
+
+type WalletBackend struct {
+	filename string
+	password string
+}
